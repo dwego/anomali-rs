764f7c97be00cbb98e9c8b2a49c5062d832b4219
Diego Araujo <dweg0epic@outlook.com>
2026-07-27 20:12:11 -0300

    feat: cli with scan subcommand contains file and column params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "anomali",
+    version,
+    about = "CLI to scan files for anomalies using benford's law"
+)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    Scan {
+        file: PathBuf,
+
+        #[arg(long)]
+        column: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Scan { file, column } => {
+            println!("File: {}", file.display());
+            println!("Column: {column}");
+        }
+    }
 }
